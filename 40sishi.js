0ed195741f940e673c6a517cfaa6972a591f310c
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-03-08 13:59:51 +0800

    Update 40sishi.js

--- 40sishi.js
+++ 40sishi.js
@@ -10,7 +10,7 @@
 
 [rewrite_local]
 
-^http[s]?:\/\/www\.40sishi\.com\/.+\/user\/profile url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/40sishi.js
+^http[s]?:\/\/www\.40sishi\.com url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/40sishi.js
 
 [mitm]
 
